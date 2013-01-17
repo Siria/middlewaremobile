b8226ec20523056ace25336d826587228c2c4ad8
alessandra.agosta@gmail.com <alessandra.agosta@gmail.com@f98e35ff-1e9c-2026-f25d-349db6b9e94e>
2013-01-17 16:37:08 +0000

    rest da testare

---
+++ MAS/src/test/ClientJerseyRest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import java.util.logging.Logger;
+
+/**
+ * Jersey REST client generated for REST resource:Evento [generic]<br>
+ *  USAGE:
+ * <pre>
+ *        ClientJerseyRest client = new ClientJerseyRest();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author alessandra
+ */
+public class ClientJerseyRest {
+    private WebResource webResource;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:17781/WebAppEvent/webresources";
+
+    public ClientJerseyRest() {
+        com.sun.jersey.api.client.config.ClientConfig config = new com.sun.jersey.api.client.config.DefaultClientConfig();
+        client = Client.create(config);
+        webResource = client.resource(BASE_URI).path("generic");
+    }
+
+    public String getText() throws UniformInterfaceException {
+        WebResource resource = webResource;
+        return resource.accept(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+    }
+
+    public void putText(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.TEXT_PLAIN).put(requestEntity);
+    }
+
+    public void close() {
+        client.destroy();
+    }
+    private static final Logger LOG = Logger.getLogger(ClientJerseyRest.class.getName());
+    
+    
+    
+    
+}
