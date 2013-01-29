e6aad7e4620872b8add2689a8e4548b1f78446e2
alessandra.agosta@gmail.com <alessandra.agosta@gmail.com@f98e35ff-1e9c-2026-f25d-349db6b9e94e>
2013-01-29 12:47:20 +0000

    app funzionante

---
+++ MAS/AppClientAle/src/java/appclientale/MessageException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appclientale;
+
+/**
+ *
+ * @author alessandra
+ */
+    public class MessageException extends Exception {
+
+/********************************************************************
+* Constructor.
+********************************************************************/
+public MessageException() {
+  super();
+}
+
+/********************************************************************
+* Constructor.
+*
+* @param info Information to be sent along with the exception.
+********************************************************************/
+public MessageException(String info) {
+  super(info);
+}
+}
