b792145f7aff2914fc29be5a1674fe416a95ddcc
s.milardo@hotmail.it <s.milardo@hotmail.it@f98e35ff-1e9c-2026-f25d-349db6b9e94e>
2013-02-12 20:31:29 +0000

    Comunicazione Tramite File Funzionante :-)

--- MAS/src/temp/ricevitori/File/RicevitoreFile.java
+++ MAS/src/temp/ricevitori/File/RicevitoreFile.java
@@ -7,6 +7,9 @@ package temp.ricevitori.File;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.util.HashMap;
 import java.util.logging.Level;
@@ -31,34 +34,54 @@ public class RicevitoreFile extends Thread implements RicevitoreProxy{
         while (true) {
          try {
             File file = new File((String)conf.get("fileIngresso"));
-            fis = new FileInputStream(file);
-            FileLock fl = fis.getChannel().tryLock();
-            boolean continua = true;
-            StringBuilder messaggio = new StringBuilder();
-            while(continua) {
-              int read = fis.read();
-              byte byteread;
-              if (read != -1){
-                  byteread = (byte)read;
-                  messaggio.append((char)(byteread));
-              } else {
-                  continua = false;
-              }
-            }
             
-            enqueue(messaggio.toString());
-            fis.close();
-        } catch (IOException ex) {
-            Logger.getLogger(TrasmettitoreFile.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                fis.close();
-            } catch (IOException ex) {
-                Logger.getLogger(TrasmettitoreFile.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }  
-        }
-    }
+                    FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
+                    boolean obtained = false;
+                    FileLock lock = null;
+                    while (!obtained){
+                    try{    
+                    lock = channel.tryLock();
+                    obtained = true;
+                    } catch (Exception e){
+                        Thread.sleep(100);
+                    }
+                    }
+                    
+                   
+                    ByteBuffer bb = ByteBuffer.allocateDirect(128*1024);
+                    StringBuilder messaggio = new StringBuilder();
+                    
+                    int nLetti = 0;
+                    int nRead;
+                    while ( (nRead=channel.read( bb )) != -1 )
+                    {
+                        nLetti++;
+                        bb.position( 0 );
+                        bb.limit( nRead );
+                        while ( bb.hasRemaining() ){
+                            messaggio.append((char)bb.get( ));
+                        }
+                        bb.clear( );
+                    }
+                    file.renameTo(new File("Rdata.dat"));
+                    //file.delete();
+                    
+                    if (nLetti > 0){
+                        System.out.println("Ricevo tramite File...");
+                        enqueue(messaggio.toString());                    
+                    }
+                    lock.release();
+		    channel.close();
+                    Thread.sleep(150);
+		}
+		catch (Exception e) {
+			System.out.println("I/O Error: " + e.getMessage());
+		}
+            
+            
+            
+       
+    }}
 
     @Override
     public void configura(Monitor monitor, HashMap conf) {
