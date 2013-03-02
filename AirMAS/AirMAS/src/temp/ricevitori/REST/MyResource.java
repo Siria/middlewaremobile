bf37ac7694e913acf92ced4227d14da1e2bd6d49
s.milardo@hotmail.it <s.milardo@hotmail.it@f98e35ff-1e9c-2026-f25d-349db6b9e94e>
2013-03-02 18:36:35 +0000

    Riorganizzazione dei package

--- AirMAS/AirMAS/src/temp/ricevitori/REST/MyResource.java
+++
@@ -1,9 +0,0 @@
-package temp.ricevitori.REST;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MyResource
-{}
